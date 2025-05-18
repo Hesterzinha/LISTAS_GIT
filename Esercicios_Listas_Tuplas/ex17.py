e7e395f7c790274b8e870bd0f44f2847bf5ff5f5
Hesterzinha <yasmin.silva8@estudante.ifms.edu.br>
2025-05-17 22:02:26 -0400

    Exercicio corrigido

---
+++ Esercicios_Listas_Tuplas/ex17.py
@@ -0,0 +1,8 @@
+lista = [10, 9, 8, 10, 9]
+
+soma = 0
+for numero in lista:
+    soma += numero
+
+media = soma / len(lista)
+print(f'A média é: {media}')
--- Esercicios_Listas_Tuplas/ex17_A_RESOLVER.py
+++
@@ -1,5 +0,0 @@
-lista = [10, 9, 8, 10, 9]
-
-for i in range():
-    media = sum(lista) / len(lista) #sum é a soma dos elementos da lista e len é o tamanho da lista
-    print(f'A média é: {media}')
